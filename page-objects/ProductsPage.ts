6f667223d1ef02c9195ce148e31a65740c995c3d
andrewnnov <andrewnnov@yandex.ru>
2023-07-29 14:10:36 -0300

    add function sort by cheapest

--- page-objects/ProductsPage.ts
+++ page-objects/ProductsPage.ts
@@ -5,12 +5,17 @@ import { Navigation } from './Navigation'
 export class ProductsPage {
   readonly page: Page
   readonly addToBasketButtons: Locator
+  readonly sortDropdown: Locator
+  readonly productTitle: Locator
+
 
   constructor(page: Page) {
     this.page = page
     this.addToBasketButtons = page.locator(
       "//button[@data-qa='product-button']",
     )
+    this.sortDropdown = page.locator("//select[@data-qa= 'sort-dropdown']")
+    this.productTitle = page.locator("//div[@data-qa= 'product-title']")
   }
 
   async visit() {
@@ -30,5 +35,19 @@ export class ProductsPage {
     await expect(specificAddButton).toHaveText('Remove from Basket')
     const basketCountAfterClick = await navigation.getBasketCount()
     expect(basketCountAfterClick).toBeGreaterThan(basketCountBeforeClick)
+  }
+
+  sortByCheapest = async () => {
+    await this.sortDropdown.waitFor()
+    //get order of products
+    await this.productTitle.first().waitFor()
+    const productTitleBeforeSorting = await this.productTitle.allInnerTexts()
+    await this.sortDropdown.selectOption("price-asc")
+    //get order of products
+    const productTitleAfterSorting = await this.productTitle.allInnerTexts()
+    //expect that these lists are different
+    expect(productTitleAfterSorting).not.toEqual(productTitleBeforeSorting)
+    await this.page.pause()
+
   }
 }
--- tests/user_full_journey.spec.ts
+++ tests/user_full_journey.spec.ts
@@ -6,6 +6,7 @@ import { Checkout } from '../page-objects/Checkout'
 test.only('User full end-to-end test journey', async ({ page }) => {
   let productsPage = new ProductsPage(page)
   await productsPage.visit()
+  await productsPage.sortByCheapest()
   await productsPage.addProductToBacket(0)
   await productsPage.addProductToBacket(1)
   await productsPage.addProductToBacket(2)
